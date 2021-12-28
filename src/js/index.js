d212a59e760dda94e51f5919b7d74010ed2424b1
Sunny <sunnydegraaf@hotmail.nl>
2021-12-28 16:24:48 +0100

    Added smooth scroll

---
+++ src/js/index.js
@@ -0,0 +1,12 @@
+document.addEventListener('click', function(event) {
+    if (!event.target.matches('.btn-scroll-into')) return;
+  
+    event.preventDefault();
+    
+    var element = document.getElementById(event.target.dataset.target);
+    var menu = document.getElementById('nav');
+    menu.checked = false
+    
+    element.scrollIntoView({block: "start", behavior: "smooth"});
+  
+  });
